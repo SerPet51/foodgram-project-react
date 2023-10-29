2fbc14a7a0749c9e3531a5018b91b4660020feb7
Сергей Петров <tetkaslepoka@yandex.ru>
2023-10-29 17:00:50 +0300

    new_server

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -131,12 +131,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.User'
 ACCOUNT_USER_MODEL_USERNAME_FIELD = 'email'
