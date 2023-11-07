9df6d8417951884de4ca4b7582a83ece1763e850
Сергей Петров <tetkaslepoka@yandex.ru>
2023-11-08 01:55:17 +0300

    review2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,10 +18,11 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['62.84.120.183',
                  '127.0.0.1',
-                 'foodgramdplm.ddns.net'
+                 'localhost',
+                 'foodgramyap.ddns.net'
                  ]
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
